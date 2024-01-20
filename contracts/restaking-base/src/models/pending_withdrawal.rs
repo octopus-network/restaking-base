a1cbf5e6257223ef37dfec4a1a499b3b0a2a8c77
Bob(xsb.near) <389483673@qq.com>
2024-01-20 11:09:06 +0800

    adjust unstake gas and adjust event fields (#16)

--- contracts/restaking-base/src/models/pending_withdrawal.rs
+++ contracts/restaking-base/src/models/pending_withdrawal.rs
@@ -77,7 +77,8 @@ impl RestakingBaseContract {
     ) -> bool {
         assert_eq!(pending_withdrawal.pool_id, staking_pool.pool_id);
         if let Some(unstake_batch_id) = pending_withdrawal.unstake_batch_id {
-            staking_pool.is_unstake_batch_withdrawn(&unstake_batch_id)
+            pending_withdrawal.is_withdrawable()
+                && staking_pool.is_unstake_batch_withdrawn(&unstake_batch_id)
         } else {
             pending_withdrawal.is_withdrawable() && staking_pool.is_withdrawable()
         }
